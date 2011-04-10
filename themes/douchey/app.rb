cc1d52fffe203851a60fe079cd1ee54c5b1133e1
Abel Rios <arios@yahoo-inc.com>
2011-04-10 00:07:52 -0500

    Adding compass to application Making sure compass works w/ Rack/Sinatra Adding reset.sass Adding text.sass Adding grid.sass Updating config to use theme Updating gems

--- themes/douchey/app.rb
+++ themes/douchey/app.rb
@@ -1,6 +1,11 @@
 # Use the app.rb file to load Ruby code, modify or extend the models, or
 # do whatever else you fancy when the theme is loaded.
 
+require 'rubygems'
+require 'compass' #must be loaded before sinatra
+require 'sinatra'
+require 'haml' #must be loaded after sinatra
+
 module Nesta
   class App
     # Uncomment the Rack::Static line below if your theme has assets
@@ -9,9 +14,35 @@ module Nesta
     # Put your assets in themes/douchey/public/douchey.
     #
     # use Rack::Static, :urls => ["/douchey"], :root => "themes/douchey/public"
+        
+    configure do
+      Compass.configuration do |config|
+        config.project_path = File.dirname(__FILE__)
+        config.sass_dir = 'views'
+      end
+
+      set :haml, { :format => :html5 }
+      set :sass, Compass.sass_engine_options
+    end
+
+    get '/css/layout.css' do
+      content_type 'text/css', :charset => 'utf-8'
+      sass :layout
+    end
 
     helpers do
       # Add new helpers here.
+      #get("/css/:name.css") do
+       # compass :one_stylesheet
+      #end
+      
+      #get '/css/:sheet.css' do
+       # puts 'In helper...'
+        #content_type 'text/css', :charset => 'utf-8'
+        #cache sass(params[:sheet].to_sym)
+      #end
+
+      
     end
 
     # Add new routes here.
