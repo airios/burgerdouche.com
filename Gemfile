4ea415d4afcdc1262d130cda804c9585585ead9b
Abel Rios <arios@yahoo-inc.com>
2011-04-11 23:01:15 -0500

    Updating theme with proper helpers

--- Gemfile
+++ Gemfile
@@ -3,6 +3,5 @@ source 'http://rubygems.org'
 gem 'nesta', '0.9.4'
 gem 'compass'
 gem 'compass-960-plugin'
-
-
+# gem 'nesta-search', '0.0.1'
 # gem (RUBY_VERSION =~ /^1.9/) ? 'ruby-debug19': 'ruby-debug'
--- themes/douchey/app.rb
+++ themes/douchey/app.rb
@@ -7,6 +7,7 @@ require 'sinatra'
 require 'haml' #must be loaded after sinatra
 
 module Nesta
+  
   class App
     # Uncomment the Rack::Static line below if your theme has assets
     # (i.e images or JavaScript).
@@ -38,8 +39,51 @@ module Nesta
 
     helpers do
       # Add new helpers here.
+      def set_common_variables
+        @menu_items = Nesta::Menu.for_path('/')
+        @site_title = Nesta::Config.title
+        @authors = Nesta::Config.authors
+        set_from_config(:title, :subtitle, :google_analytics_code)
+        @heading = @title
+      end
+      
+      def author_uri(author)
+        get_author_data_by_key(author,"uri")
+      end
+      
+      def author_adsense_id(author)
+        get_author_data_by_key(author,"adsenseid")
+      end
+      
+      def author_email(author)
+        get_author_data_by_key(author,"email")
+      end
+      
+      def get_author_data_by_key(author,key)
+        @authors.each do |a|
+          if a[ "name" ].to_s.downcase == author.downcase
+            return a[ key ]
+          end
+        end
+      end
     end
-
+   
     # Add new routes here.
+  end
+  
+  class Config
+    def self.authors
+      authors = from_yaml("authors")
+    end
+  end
+  
+  class FileModel
+    def author
+      if metadata("author")
+        metadata("author")
+      else
+        Nesta::Config.author["name"]
+      end
+    end
   end
 end
